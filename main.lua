2cf7ec9fce7e1c5e2f908b0801699d68a2b2583a
Delphine Shi <shi.delphine@gmail.com>
2019-01-15 21:46:31 +0100

    Add falling enemies

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+enemy = {}
+enemies_controller = {}
+enemies_controller.enemies = {}
+
 function love.load()
     player = {}
     player.x = 0
@@ -19,6 +23,33 @@ function love.load()
             table.insert(player.bullets, bullet)
         end
     end
+    enemies_controller:spawnEnemy(0, 0)
+    enemies_controller:spawnEnemy(40, 0)
+end
+
+function enemies_controller:spawnEnemy(x, y)
+    enemy = {}
+    enemy.x = x
+    enemy.y = y
+    enemy.width = 30
+    enemy.height = 30
+    enemy.bullets = {}
+    enemy.cooldown_ref = 50
+    enemy.cooldown = 50
+    enemy.speed = 2
+    table.insert(self.enemies, enemy)
+end
+
+function enemy:fire()
+    if self.cooldown <= 0 then
+        self.cooldown = player.cooldown_ref
+        bullet = {}
+        bullet.width = 10
+        bullet.height = 10
+        bullet.x = self.x + self.width/2 - self.width/2
+        bullet.y = self.y
+        table.insert(self.bullets, bullet)
+    end
 end
 
 function love.update(dt)
@@ -33,6 +64,10 @@ function love.update(dt)
         player.fire()
     end
 
+    for _,enemy in pairs(enemies_controller.enemies) do
+        enemy.y = enemy.y + 0.3
+    end
+
     for i,bullet in ipairs(player.bullets) do
         if bullet.y < -10 then
             table.remove(player.bullets, i)
@@ -45,6 +80,12 @@ function love.draw()
     -- draw the player
     love.graphics.setColor(255, 0, 0)
     love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+
+    -- draw enemies
+    love.graphics.setColor(0, 0, 255)
+    for _,enemy in pairs(enemies_controller.enemies) do
+        love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
+    end
 
     -- draw bullets
     love.graphics.setColor(255, 255, 255)
